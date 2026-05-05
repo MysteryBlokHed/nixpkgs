f8171131124d5fa3d2e761ced8c773155689669e
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2026-05-04 22:24:01 -0400

    work in progress

--- pkgs/by-name/op/opentabletdriver/package.nix
+++ pkgs/by-name/op/opentabletdriver/package.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  stdenvNoCC,
   buildDotnetModule,
   copyDesktopItems,
   coreutils,
@@ -20,122 +21,30 @@
   nix-update-script,
   udevCheckHook,
 }:
-
-buildDotnetModule (finalAttrs: {
+let
   pname = "OpenTabletDriver";
   version = "0.6.7";
 
   src = fetchFromGitHub {
     owner = "OpenTabletDriver";
     repo = "OpenTabletDriver";
-    tag = "v${finalAttrs.version}";
+    tag = "v${version}";
     hash = "sha256-jL3d1DjY9n85BrO6ajZVvJMHmPYfxng4YE25s/9hfGA=";
   };
 
   dotnet-sdk = dotnetCorePackages.sdk_8_0;
 
-  projectFile = [
-    "OpenTabletDriver.Console"
-    "OpenTabletDriver.Daemon"
-    "OpenTabletDriver.UX.Gtk"
-  ];
-  nugetDeps = ./deps.json;
+    env.OTD_CONFIGURATIONS = "${src}/OpenTabletDriver.Configurations/Configurations";
+    env.SELF_CONTAINED = stdenvNoCC.hostPlatform.isDarwin;
 
-  executables = [
-    "OpenTabletDriver.Console"
-    "OpenTabletDriver.Daemon"
-    "OpenTabletDriver.UX.Gtk"
-  ];
-
-  nativeBuildInputs = [
-    copyDesktopItems
-    wrapGAppsHook3
-    udevCheckHook
-    # Dependency of generate-rules.sh
-    jq
-  ];
-
-  runtimeDeps = [
-    gtk3
-    libappindicator
-    libevdev
-    libnotify
-    libx11
-    libxrandr
-    udev
-  ];
-
-  buildInputs = finalAttrs.runtimeDeps;
-
-  env.OTD_CONFIGURATIONS = "${finalAttrs.src}/OpenTabletDriver.Configurations/Configurations";
-
-  doCheck = true;
-  testProjectFile = "OpenTabletDriver.Tests/OpenTabletDriver.Tests.csproj";
-
-  disabledTests = [
-    # Require networking & unused in Linux build
-    "OpenTabletDriver.Tests.UpdaterTests.CheckForUpdates_Returns_Update_When_Available"
-    "OpenTabletDriver.Tests.UpdaterTests.Install_Throws_UpdateAlreadyInstalledException_When_AlreadyInstalled"
-    "OpenTabletDriver.Tests.UpdaterTests.Install_DoesNotThrow_UpdateAlreadyInstalledException_When_PreviousInstallFailed"
-    "OpenTabletDriver.Tests.UpdaterTests.Install_Throws_UpdateInProgressException_When_AnotherUpdate_Is_InProgress"
-    "OpenTabletDriver.Tests.UpdaterTests.Install_Moves_UpdatedBinaries_To_BinDirectory"
-    "OpenTabletDriver.Tests.UpdaterTests.Install_Moves_Only_ToBeUpdated_Binaries"
-    "OpenTabletDriver.Tests.UpdaterTests.Install_Copies_AppDataFiles"
-    # Depends on processor load
-    "OpenTabletDriver.Tests.TimerTests.TimerAccuracy"
-  ];
-
-  preBuild = ''
-    patchShebangs generate-rules.sh
-    substituteInPlace generate-rules.sh \
-      --replace-fail '/usr/bin/env rm' '${lib.getExe' coreutils "rm"}'
-  '';
-
-  dontWrapGApps = true;
-
-  postFixup = ''
-    # Give a more "*nix" name to the binaries
-    mv $out/bin/OpenTabletDriver.Console $out/bin/otd
-    mv $out/bin/OpenTabletDriver.Daemon $out/bin/otd-daemon
-    mv $out/bin/OpenTabletDriver.UX.Gtk $out/bin/otd-gui
-
-    install -Dm644 $src/OpenTabletDriver.UX/Assets/otd.png -t $out/share/icons
-
-    # Generate udev rules from source
-    mkdir -p $out/lib/udev/rules.d
-    ./generate-rules.sh > $out/lib/udev/rules.d/70-opentabletdriver.rules
-
-    wrapProgram $out/bin/otd-gui \
-      "''${gappsWrapperArgs[@]}" \
-      --add-flags --skipupdate
-  '';
-
-  desktopItems = [
-    (makeDesktopItem {
-      desktopName = "OpenTabletDriver";
-      name = "OpenTabletDriver";
-      exec = "otd-gui";
-      icon = "otd";
-      comment = "Open source, cross-platform, user-mode tablet driver";
-      categories = [ "Utility" ];
-    })
-  ];
-
-  doInstallCheck = true;
-  nativeInstallCheckInputs = [
-    versionCheckHook
-  ];
-  versionCheckProgram = "${placeholder "out"}/bin/otd-daemon";
-
-  passthru = {
-    updateScript = nix-update-script { };
-    tests = {
-      otd-runs = nixosTests.opentabletdriver;
-    };
-  };
+    preBuild = ''
+      patchShebangs generate-rules.sh
+      substituteInPlace generate-rules.sh \
+        --replace-fail '/usr/bin/env rm' '${lib.getExe' coreutils "rm"}'
+    '';
 
   meta = {
-    changelog = "https://github.com/OpenTabletDriver/OpenTabletDriver/releases/tag/v${finalAttrs.version}";
+    changelog = "https://github.com/OpenTabletDriver/OpenTabletDriver/releases/tag/v${version}";
     description = "Open source, cross-platform, user-mode tablet driver";
     homepage = "https://github.com/OpenTabletDriver/OpenTabletDriver";
     license = lib.licenses.lgpl3Plus;
@@ -147,6 +56,173 @@ buildDotnetModule (finalAttrs: {
     platforms = [
       "x86_64-linux"
       "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
     ];
   };
-})
+
+  darwin = buildDotnetModule (finalAttrs: {
+    inherit
+      pname
+      version
+      src
+      dotnet-sdk
+      preBuild
+      meta
+      ;
+
+  postFetch = ''
+    find . -type f -exec sed -i 's/osx-x64/osx-arm64/g' {} +
+  '';
+
+    projectFile = [
+      "OpenTabletDriver.Console"
+      "OpenTabletDriver.Daemon"
+      "OpenTabletDriver.UX.MacOS"
+    ];
+    nugetDeps = ./deps-darwin.json;
+
+    executables = [
+      "OpenTabletDriver.Console"
+      "OpenTabletDriver.Daemon"
+      "OpenTabletDriver.UX.MacOS"
+    ];
+
+    nativeBuildInputs = [
+      copyDesktopItems
+      wrapGAppsHook3
+    ];
+
+    runtimeId = "osx-arm64";
+    runtimeDeps = [ libnotify ];
+
+    buildInputs = finalAttrs.runtimeDeps;
+
+    doCheck = false;
+
+    dontWrapGApps = true;
+
+  postFixup = ''
+  ls -al
+  ls -al $out
+  ls -al $out/bin
+  ls -al $out/lib
+
+      mv $out/bin/OpenTabletDriver.Console $out/bin/otd
+      mv $out/bin/OpenTabletDriver.Daemon $out/bin/otd-daemon
+      mv $out/bin/OpenTabletDriver.UX.MacOS $out/bin/otd-gui
+
+      wrapProgram $out/bin/otd-gui \
+        "''${gappsWrapperArgs[@]}" \
+        --add-flags --skipupdate
+  '';
+
+    passthru = {
+      updateScript = nix-update-script { };
+    };
+  });
+
+  linux = buildDotnetModule (finalAttrs: {
+    inherit
+      pname
+      version
+      src
+      dotnet-sdk
+      env
+      preBuild
+      meta
+      ;
+
+    projectFile = [
+      "OpenTabletDriver.Console"
+      "OpenTabletDriver.Daemon"
+      "OpenTabletDriver.UX.Gtk"
+    ];
+    nugetDeps = ./deps-linux.json;
+
+    # executables = [
+      # "OpenTabletDriver.Console"
+      # "OpenTabletDriver.Daemon"
+      # "OpenTabletDriver.UX.Gtk"
+    # ];
+
+    nativeBuildInputs = [
+      wrapGAppsHook3
+      udevCheckHook
+      # Dependency of generate-rules.sh
+      jq
+    ];
+
+    runtimeDeps = [
+      gtk3
+      libappindicator
+      libevdev
+      libnotify
+      libx11
+      libxrandr
+      udev
+    ];
+
+    buildInputs = finalAttrs.runtimeDeps;
+
+    doCheck = true;
+    testProjectFile = "OpenTabletDriver.Tests/OpenTabletDriver.Tests.csproj";
+
+    disabledTests = [
+      # Require networking & unused in Linux build
+      "OpenTabletDriver.Tests.UpdaterTests.CheckForUpdates_Returns_Update_When_Available"
+      "OpenTabletDriver.Tests.UpdaterTests.Install_Throws_UpdateAlreadyInstalledException_When_AlreadyInstalled"
+      "OpenTabletDriver.Tests.UpdaterTests.Install_DoesNotThrow_UpdateAlreadyInstalledException_When_PreviousInstallFailed"
+      "OpenTabletDriver.Tests.UpdaterTests.Install_Throws_UpdateInProgressException_When_AnotherUpdate_Is_InProgress"
+      "OpenTabletDriver.Tests.UpdaterTests.Install_Moves_UpdatedBinaries_To_BinDirectory"
+      "OpenTabletDriver.Tests.UpdaterTests.Install_Moves_Only_ToBeUpdated_Binaries"
+      "OpenTabletDriver.Tests.UpdaterTests.Install_Copies_AppDataFiles"
+      # Depends on processor load
+      "OpenTabletDriver.Tests.TimerTests.TimerAccuracy"
+    ];
+
+    dontWrapGApps = true;
+
+    postFixup = ''
+      # Give a more "*nix" name to the binaries
+      mv $out/bin/OpenTabletDriver.Console $out/bin/otd
+      mv $out/bin/OpenTabletDriver.Daemon $out/bin/otd-daemon
+      mv $out/bin/OpenTabletDriver.UX.Gtk $out/bin/otd-gui
+
+      install -Dm644 $src/OpenTabletDriver.UX/Assets/otd.png -t $out/share/icons
+
+      # Generate udev rules from source
+      mkdir -p $out/lib/udev/rules.d
+      ./generate-rules.sh > $out/lib/udev/rules.d/70-opentabletdriver.rules
+
+      wrapProgram $out/bin/otd-gui \
+        "''${gappsWrapperArgs[@]}" \
+        --add-flags --skipupdate
+    '';
+
+    desktopItems = [
+      (makeDesktopItem {
+        desktopName = "OpenTabletDriver";
+        name = "OpenTabletDriver";
+        exec = "otd-gui";
+        icon = "otd";
+        comment = "Open source, cross-platform, user-mode tablet driver";
+        categories = [ "Utility" ];
+      })
+    ];
+
+    doInstallCheck = true;
+    nativeInstallCheckInputs = [
+      versionCheckHook
+    ];
+    versionCheckProgram = "${placeholder "out"}/bin/otd-daemon";
+
+    passthru = {
+      updateScript = nix-update-script { };
+      tests = {
+        otd-runs = nixosTests.opentabletdriver;
+      };
+    };
+  });
+in
+if stdenvNoCC.hostPlatform.isDarwin then darwin else linux
